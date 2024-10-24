54330c2e299ae665a6d0a278358fae1841fe60db
antondev-123 <sydoranton99@outlook.com>
2024-10-24 06:46:23 +0300

    update
    Co-Authored-By: Anton Sydor <175124793+Dev-Star0102@users.noreply.github.com>

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,49 +1,45 @@
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
-import AppBar from './AppBar'
-import { useContext, useEffect } from 'react'
-import { AuthContext } from '@/context/AuthProvider'
-import FloatingNav from './FloatingBar'
-import SideBar from './Sidebar'
-import SignalLight from '../signal/SignalLight'
-import { SignalContext } from '@/context/SignalProvider'
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import AppBar from "./AppBar";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "@/context/AuthProvider";
+import FloatingNav from "./FloatingBar";
+import SideBar from "./Sidebar";
+import SignalLight from "../signal/SignalLight";
+import { SignalContext } from "@/context/SignalProvider";
 
 export default function Layout({ nav }: { nav?: boolean }) {
-  const location = useLocation()
-  const authPages = ['sign-in', 'sign-up']
-  const hideAppBar = authPages.includes(location.pathname.split('/')[1])
-  const navigate = useNavigate()
-  const { currentUser } = useContext(AuthContext)
-  const { signal } = useContext(SignalContext)
+  const location = useLocation();
+  const authPages = ["sign-in", "sign-up"];
+  const hideAppBar = authPages.includes(location.pathname.split("/")[1]);
+  const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
+  const { signal } = useContext(SignalContext);
 
   useEffect(() => {
-    if (location.pathname == '/') {
-      navigate('/home')
+    if (location.pathname == "/") {
+      navigate("/home");
     }
 
     if (currentUser && hideAppBar) {
-      navigate('/home')
+      navigate("/home");
     }
 
-    if(currentUser && Notification.permission === 'default') {
-      Notification.requestPermission()
+    if (currentUser && Notification.permission === "default") {
+      Notification.requestPermission();
     }
-    
-  }, [navigate, location.pathname, hideAppBar, currentUser])
+  }, [navigate, location.pathname, hideAppBar, currentUser]);
 
   return (
-    <div className='w-full h-full'>
+    <div className="w-full h-full">
       {!hideAppBar && <AppBar />}
       {!hideAppBar && !nav && <FloatingNav />}
       {nav ? (
-        <div className='flex h-full pt-16'>
+        <div className="flex h-full pt-16">
           {signal && <SignalLight />}
-          <nav className='fixed h-full w-[280px] border-r border-gray-300'>
+          <nav className="fixed h-full w-[280px] border-r border-gray-300">
             <SideBar />
           </nav>
-          <main
-            className='ml-[280px]'
-            style={{ width: 'calc(100% - 280px)' }}
-          >
+          <main className="ml-[280px]" style={{ width: "calc(100% - 280px)" }}>
             <Outlet />
           </main>
         </div>
@@ -51,5 +47,5 @@ export default function Layout({ nav }: { nav?: boolean }) {
         <Outlet />
       )}
     </div>
-  )
+  );
 }
