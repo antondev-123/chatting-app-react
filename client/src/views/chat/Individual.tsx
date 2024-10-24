a6d47ff2d80eb05aef62e580a08931d430cd967f
Dev-Star0102 <lucky.dev0102@gmail.com>
2024-10-24 06:51:27 +0300

    update
    Co-Authored-By: Anton <178762083+antondev-123@users.noreply.github.com>

--- client/src/views/chat/Individual.tsx
+++ client/src/views/chat/Individual.tsx
@@ -1,51 +1,52 @@
-import Message from '@/components/chat/Message'
-import MessageInput from '@/components/chat/MessageInput'
-import { AuthContext } from '@/context/AuthProvider'
-import { SignalContext } from '@/context/SignalProvider'
-import type { Message as MessageModel } from '@/models/message'
-import { User } from '@/models/user'
-import { subscribeToSpecificMessages } from '@/services/chatting'
-import { useContext, useEffect, useRef, useState } from 'react'
-import { useLoaderData, useParams } from 'react-router-dom'
+import Message from "@/components/chat/Message";
+import MessageInput from "@/components/chat/MessageInput";
+import { AuthContext } from "@/context/AuthProvider";
+import { SignalContext } from "@/context/SignalProvider";
+import type { Message as MessageModel } from "@/models/message";
+import { User } from "@/models/user";
+import { subscribeToSpecificMessages } from "@/services/chatting";
+import { useContext, useEffect, useRef, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const Individual = () => {
-  const { email } = useParams()
-  const privateBoxRef = useRef<HTMLDivElement | null>(null)
-  const [messages, setMessages] = useState<MessageModel[]>()
-  const { currentUser } = useContext(AuthContext)
-  const { setSignal } =useContext(SignalContext)
-  const user = useLoaderData() as User
+  const { email } = useParams();
+  const privateBoxRef = useRef<HTMLDivElement | null>(null);
+  const [messages, setMessages] = useState<MessageModel[]>();
+  const { currentUser } = useContext(AuthContext);
+  const { setSignal } = useContext(SignalContext);
+  const user = useLoaderData() as User;
 
   useEffect(() => {
-    if(privateBoxRef.current) {
-      privateBoxRef.current.lastElementChild?.scrollIntoView()
+    if (privateBoxRef.current) {
+      privateBoxRef.current.lastElementChild?.scrollIntoView();
     }
-  }, [privateBoxRef.current?.lastElementChild])
+  }, [privateBoxRef.current?.lastElementChild]);
 
   useEffect(() => {
     if (email) {
-      const unSubscribe = subscribeToSpecificMessages(setMessages, {
-        sender: currentUser!.email as string,
-        receiver: email as string,
-      }, setSignal)
-      return () => unSubscribe()
+      const unSubscribe = subscribeToSpecificMessages(
+        setMessages,
+        {
+          sender: currentUser!.email as string,
+          receiver: email as string
+        },
+        setSignal
+      );
+      return () => unSubscribe();
     }
-  }, [email, currentUser, setSignal])
+  }, [email, currentUser, setSignal]);
 
   return (
     <>
-      <div className='individual m-0'>
-        <header className='individual__header '>
-          <h2 className='individual__title text-center text-xl font-bold p-2'>
+      <div className="individual m-0">
+        <header className="individual__header ">
+          <h2 className="individual__title text-center text-xl font-bold p-2">
             Personal Chat
           </h2>
           <hr />
         </header>
 
-        <section
-          className='message-box p-4'
-          aria-description='message box'
-        >
+        <section className="message-box p-4" aria-description="message box">
           <div ref={privateBoxRef}>
             {messages?.map((message) => {
               return (
@@ -58,27 +59,29 @@ const Individual = () => {
                       : message.sender_email.charAt(0).toUpperCase()
                   }
                   direction={
-                    currentUser?.email === message.sender_email ? 'right' : 'left'
+                    currentUser?.email === message.sender_email
+                      ? "right"
+                      : "left"
                   }
                 />
-              )
+              );
             })}
           </div>
         </section>
 
         <section
-          className='fixed bottom-0 p-4 bg-white'
-          style={{ width: 'calc(100% - 280px)' }}
+          className="fixed bottom-0 p-4 bg-white"
+          style={{ width: "calc(100% - 280px)" }}
         >
           <MessageInput
-            type='personal'
+            type="personal"
             receiverEmail={user.email}
             receiverName={user.name}
           />
         </section>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Individual
+export default Individual;
